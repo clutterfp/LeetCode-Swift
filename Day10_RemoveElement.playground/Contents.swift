3875935d20780c48f1af150b34c2243d72c9bd7f
BreadAwesome <AnotherChase@gmail.com>
2018-01-05 10:15:59 +0800

    Day10：Remove Element

---
+++ Day10_RemoveElement.playground/Contents.swift
@@ -0,0 +1,32 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+/**
+ Remove Element
+ 
+ Given an array and a value, remove all instances of that value in-place and return the new length.
+ 
+ Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+ 
+ The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+ 
+ Example:
+ Given nums = [3,2,2,3], val = 3,
+ Your function should return length = 2, with the first two elements of nums being 2.
+ **/
+
+class Solution {
+    func removeElement(_ nums: inout [Int], _ val: Int) -> Int {
+        var index = nums.count - 1
+        for num in nums.reversed() {
+            if num == val {
+                nums.remove(at: index)
+            }
+            
+            index -= 1
+        }
+        
+        return nums.count
+    }
+}

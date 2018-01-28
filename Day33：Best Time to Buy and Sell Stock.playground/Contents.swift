793c3499389f2827d4ddc57b2086fd7f318360e3
BreadAwesome <AnotherChase@gmail.com>
2018-01-28 19:08:38 +0800

    Day33： Best Time to Buy and Sell Stock

---
+++ Day33：Best Time to Buy and Sell Stock.playground/Contents.swift
@@ -0,0 +1,45 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+/**
+ Best Time to Buy and Sell Stock
+ 
+ Say you have an array for which the ith element is the price of a given stock on day i.
+ 
+ If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+ 
+ Example 1:
+ Input: [7, 1, 5, 3, 6, 4]
+ Output: 5
+ 
+ max. difference = 6-1 = 5 (not 7-1 = 6, as selling price needs to be larger than buying price)
+ 
+ Example 2:
+ Input: [7, 6, 4, 3, 1]
+ Output: 0
+ 
+ In this case, no transaction is done, i.e. max profit = 0.
+ **/
+
+class Solution {
+    func maxProfit(_ prices: [Int]) -> Int {
+        var i = 0
+        var max = 0
+        var count = 0
+        while i < prices.count - 1 {
+            count = count + prices[i + 1] - prices[i]
+            if count < 0 {
+                count = 0
+            }
+            
+            if count > 0 && count > max {
+                max = count
+            }
+            
+            i += 1
+        }
+        
+        return max
+    }
+}

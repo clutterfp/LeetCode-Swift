e287e02289ec5f49a9ca8bf6ee43f1b61b479f59
BreadAwesome <AnotherChase@gmail.com>
2018-02-20 21:41:02 +0800

    Day56：Reverse String

---
+++ Day56_Reverse String.playground/Contents.swift
@@ -0,0 +1,16 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+/**
+ Write a function that takes a string as input and returns the string reversed.
+ 
+ Example:
+ Given s = "hello", return "olleh".
+ **/
+
+class Solution {
+    func reverseString(_ s: String) -> String {
+        return String(s.reversed())
+    }
+}

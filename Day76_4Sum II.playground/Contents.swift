881ddf0131b03cebe30b08d7aeaaf4ed514369ef
BreadAwesome <AnotherChase@gmail.com>
2018-03-12 10:53:16 +0800

    Day76：4Sum II

---
+++ Day76_4Sum II.playground/Contents.swift
@@ -0,0 +1,47 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+/**
+ 4Sum II
+ 
+ Given four lists A, B, C, D of integer values, compute how many tuples (i, j, k, l) there are such that A[i] + B[j] + C[k] + D[l] is zero.
+ 
+ To make problem a bit easier, all A, B, C, D have same length of N where 0 ≤ N ≤ 500. All integers are in the range of -228 to 228 - 1 and the result is guaranteed to be at most 231 - 1.
+ 
+ Example:
+ 
+ Input:
+ A = [ 1, 2]
+ B = [-2,-1]
+ C = [-1, 2]
+ D = [ 0, 2]
+ 
+ Output:
+ 2
+ 
+ Explanation:
+ The two tuples are:
+ 1. (0, 0, 0, 1) -> A[0] + B[0] + C[0] + D[1] = 1 + (-2) + (-1) + 2 = 0
+ 2. (1, 1, 0, 0) -> A[1] + B[1] + C[0] + D[0] = 2 + (-1) + (-1) + 0 = 0
+ **/
+
+class Solution {
+    func fourSumCount(_ A: [Int], _ B: [Int], _ C: [Int], _ D: [Int]) -> Int {
+        var map = [Int : Int]()
+        for c in C {
+            for d in D {
+                map[c + d] = (map[c + d] ?? 0) + 1
+            }
+        }
+        
+        var count = 0
+        for a in A {
+            for b in B {
+                count += map[-1 * (a + b)] ?? 0
+            }
+        }
+        
+        return count
+    }
+}
